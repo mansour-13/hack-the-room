d7377c2915596eaadf40274440b34f26fde68763
biocarl <bioflash1@web.de>
2023-09-12 14:13:08 +0200

    Add environments

--- src/app/hello-world.service.ts
+++ src/app/hello-world.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import { environment } from './../environments/environment';
+
 
 // The interface needs to be exported, so that it can be imported in other parts of the application
 export interface Greeting{
@@ -11,10 +13,9 @@ export interface Greeting{
   providedIn: 'root'
 })
 export class HelloWorldService {
-  baseUrl = "http://localhost:8080";
   constructor(private client : HttpClient) { }
 
   getGreeting() : Observable<Greeting>{
-    return this.client.get<Greeting>(this.baseUrl + "/greeting");
+    return this.client.get<Greeting>(environment.baseUrl + "/greeting");
   }
 }
