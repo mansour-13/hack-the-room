b6a77fb60b85ad153cdd8b2d81f2b0d04f7728b5
Mansour <mansour.mazaheri@gmail.com>
2023-09-14 14:30:29 +0200

    home page component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
-import {EscapeRoomComponent} from "./escape-room/escape-room.component";
 
 const routes: Routes = [
   {path : "", component: HomeComponent},
   {path : "about", component: AboutComponent},
   {path : "login", component: LoginComponent},
-  {path : "register", component: RegisterComponent},
-  {path:'escape-room', component:EscapeRoomComponent}
+  {path : "register", component: RegisterComponent}
+
 ];
 
 @NgModule({
