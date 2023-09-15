47bb7f392ecdfcf4c6c3f21784dfc527c027d033
Mansour <mansour.mazaheri@gmail.com>
2023-09-15 16:15:32 +0200

    added if statement to RegistrationController

--- src/main/java/test/server/demo/RegistrationController.java
+++ src/main/java/test/server/demo/RegistrationController.java
@@ -7,19 +7,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import test.server.demo.security.MyUserDetailsService;
 import test.server.demo.user.User;
+import test.server.demo.user.UserRepository;
+
+import java.util.Optional;
 
 @RestController
 public class RegistrationController {
 
     private final MyUserDetailsService userDetailsService;
+    private final UserRepository userRepository;
 
-    public RegistrationController(MyUserDetailsService userDetailsService) {
+    public RegistrationController(MyUserDetailsService userDetailsService, UserRepository userRepository) {
         this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User newUser) {
-        User user = userDetailsService.saveUser(newUser);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    public ResponseEntity<?> register(@RequestBody User newUser) {
+
+        Optional<User> existingUser = userRepository.findByUserName(newUser.getUserName());
+
+        if (existingUser.isPresent()) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.BAD_REQUEST);
+        } else {
+            User user = userDetailsService.saveUser(newUser);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }
     }
 }
