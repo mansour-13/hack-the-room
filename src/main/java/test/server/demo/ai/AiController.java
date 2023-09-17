a7ecf2adea6380ace31ce22e02b4d23c5999e422
Philipp987 <philipp-wagner91@web.de>
2023-09-17 22:01:38 +0200

    Working on implementing chatgpt response on a postmapping

--- src/main/java/test/server/demo/ai/AiController.java
+++ src/main/java/test/server/demo/ai/AiController.java
@@ -1,9 +1,7 @@
 package test.server.demo.ai;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AiController {
@@ -19,5 +17,11 @@ public class AiController {
     @GetMapping("/fortuneCookie")
     public String getResult(@RequestParam(defaultValue = "Xena") String name){
         return this.aiService.prompt(String.format("Create a furtune cookie quote which %s should read. Make it funny and short (max 20 words).", name));
+    }
+
+    @PostMapping("/evaluateCode")
+    public String evaluateCode(@RequestBody String code) {
+        String promptToEvaluate = "Evaluate the given code for correctness: " + code + ". Give feedback on improvements.";
+        return this.aiService.prompt(promptToEvaluate);
     }
 }
