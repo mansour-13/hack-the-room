5c2738fd2f71ef420967cca197ec1fbf5f3a2265
Mansour <mansour.mazaheri@gmail.com>
2023-09-20 12:10:40 +0200

    added user Component and user Service

---
+++ src/app/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../environments/environment";
+import {AuthService} from "./auth.service";
+
+export interface User {
+  id: number;
+  userName: string;
+  password: string;
+  active: boolean;
+  roles: string;
+  idxActualLearnObject: number;
+  life: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private username?: string;
+
+  constructor(private client: HttpClient, private authService: AuthService) {
+    this.username = authService.getUsername();
+  }
+
+  getUserByUsername(username: string): Observable<User> {
+    return this.client.get<User>(environment.baseUrl + "/user/" + username);
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {User, UserService} from "../user.service";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  user?: User;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    const username = 'user1';
+    this.userService.getUserByUsername(username).subscribe(
+      (response) => {
+        this.user = response;
+      },
+      (error) => {
+        console.error('Error fetching user data:', error);
+      }
+    );
+  }
+}
