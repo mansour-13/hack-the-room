06294aa2e372309300f0139bce61bccbf5b1ef05
Mansour <mansour.mazaheri@gmail.com>
2023-09-21 13:38:20 +0200

    changed the display of escape-room component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {User, UserService} from "../user.service";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-user',
@@ -9,11 +10,12 @@ import {User, UserService} from "../user.service";
 export class UserComponent implements OnInit {
   user?: User;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+              private authService: AuthService) {
   }
 
   ngOnInit(): void {
-    const username = 'user1';
+    const username = this.authService.getUsername();
     this.userService.getUserByUsername(username).subscribe(
       (response) => {
         this.user = response;
