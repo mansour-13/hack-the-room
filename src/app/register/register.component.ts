c3ccd7d1ab78517ae0af866522704b712af7dda2
AlLe <Alexander.Leibold@brightstraining.com>
2023-09-22 08:35:51 +0200

    register timeout changed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -29,7 +29,7 @@ export class RegisterComponent {
           this.info = "Registered Successfully";
           setTimeout(() => {
             this.router.navigate(["/login"]);
-          }, 3000);
+          }, 1000);
         },
         error: err => {
           this.info = err.error;
